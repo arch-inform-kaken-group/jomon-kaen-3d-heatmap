6e3a43a2f7272c43ed250d60f3b364a09223a5cc
luhouyang <luhouyang@gmail.com>
2025-07-20 22:40:36 +0800

    other device paths

--- src/dataset/testing_use.py
+++ src/dataset/testing_use.py
@@ -1,22 +1,43 @@
 from utils import *
-
+import time
 
 def main():
+    st = time.time_ns()
+
+    # data, errors = filter_data_on_condition(
+    #     root="./src/data",
+    #     pottery_path="./src/pottery",
+    #     hololens_2_spatial_error=1.5,
+    #     preprocess=True,
+    #     use_cache=False,
+    #     # 'HEATMAP, QNA, VOICE': 0 | 'HEATMAP, QNA': 1 | 'HEATMAP, VOICE': 2 | 'HEATMAP': 3
+    #     mode=0,
+    #     # generate_pc_hm_voxel=False,
+    #     # generate_qna=False,
+    #     # generate_voice=True,
+    #     # generate_pottery_dogu_voxel=False,
+    # )
+
+
     data, errors = filter_data_on_condition(
-        root="./src/data",
-        pottery_path="./src/pottery",
+        root=r"D:\storage\jomon_kaen\data",
+        pottery_path=r"D:\storage\jomon_kaen\pottery",
         hololens_2_spatial_error=1.5,
         preprocess=True,
-        use_cache=False,
+        use_cache=True,
         # 'HEATMAP, QNA, VOICE': 0 | 'HEATMAP, QNA': 1 | 'HEATMAP, VOICE': 2 | 'HEATMAP': 3
         mode=0,
         # generate_pc_hm_voxel=False,
         # generate_qna=False,
-        # generate_voice=True,
+        generate_voice=True,
         # generate_pottery_dogu_voxel=False,
     )
 
     # print(errors, len(data))
+
+    et = time.time_ns()
+
+    print(f"TIME: {(et-st)/1e9}")
 
 
 if "__main__" == __name__:
