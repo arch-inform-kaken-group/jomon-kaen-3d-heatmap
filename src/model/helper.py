1d2292009361c95519c95fb7d98bb025eaf7d8d0
Lu Hou Yang <luhouyang@gmail.com>
2025-10-29 00:23:10 +0900

    helper

--- src/model/helper.py
+++ src/model/helper.py
@@ -38,7 +38,6 @@ class SimpleTokenizer:
         tokenizer, mode = _get_sudachi_tokenizer()
         tokens = [
             m.surface()
-            m.surface()
             for m in tokenizer.tokenize(normalized, mode)
         ]
         return tokens
